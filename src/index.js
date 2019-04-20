d2cd0caad26d3fc13798c42479f44a8ebdb7fff0
Stephen Woods <stephen.woods@salesforce.com>
2019-04-20 12:29:33 -0700

    add syntax highlighting

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ const codeFrameColumns = require('@babel/code-frame').codeFrameColumns;
 const button = document.querySelector('button');
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
+const hljs = require('highlight.js/lib/highlight.js');
+const js = require('highlight.js/lib/languages/javascript');
+hljs.registerLanguage('javascript', js);
 button.addEventListener('click', e => {
     button.classList.add('is-loading');
     z(input.value).then(result => {
         output.textContent = result.code;
-        output.scrollIntoView();
+        hljs.highlightBlock(output);
+        output.classList.remove('is-hidden');
         button.classList.remove('is-loading');
+        output.scrollIntoView();
     }, err => {
         output.textContent = 'Parse error:\n' + codeFrameColumns(input.value, err.loc, {message: err.message});
         button.classList.remove('is-loading');
+        output.classList.remove('is-hidden');
     });
 });
