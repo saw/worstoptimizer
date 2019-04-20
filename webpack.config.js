4099ee5eb8850bcf8bac9da862e919e7a9f0c115
Stephen Woods <stephen.woods@salesforce.com>
2019-04-20 16:34:24 -0700

    a little less css

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,18 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
-    watch: true,
+    // watch: true,
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'out.[chunkhash].js'
     },
     plugins:[
         new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
             filename: '[name].[chunkhash].css',
 
         }),
         new HtmlWebpackPlugin({
             title: 'Premature Optimization',
-            // Load a custom template (lodash by default)
             template: path.resolve(__dirname, 'src', 'index.html'),
         }),
         new CopyPlugin([
@@ -32,16 +29,10 @@ module.exports = {
             use: [
                 {
                     loader: MiniCssExtractPlugin.loader,
-                    options: {
-                      // you can specify a publicPath here
-                      // by default it uses publicPath in webpackOptions.output
-                    //   publicPath:path.resolve(__dirname, 'public'),
-                    //   hmr: process.env.NODE_ENV === 'development',
-                    },
             
                 },
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                "css-loader",
+                "sass-loader"
             ]
         }]
     }
